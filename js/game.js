a7c6f89a657b81aa2deb903f2cbbf495789d58dd
James <jamesncoupland@gmail.com>
2021-09-27 21:37:02 +0100

    palyround shows winner

--- js/game.js
+++ js/game.js
@@ -31,36 +31,30 @@ function playerSelection() {
 
 //declare winner after turn 
 //scores if player wins add 1 to current score both srtart at 0
-function playRound() {
+function playRound(playerSelection, computerPlay) {
+       
 
-     let draw, playerWin, cpuWin;
+  if (choice === "rock" && cpuMove === "rock")console.log("draw");
+   if (choice === "paper" && cpuMove === "paper")console.log("draw");
+   if (choice === "scissors" && cpuMove === "scissors")console.log("draw");
     
 
-   if (choice === "rock" && cpuMove === "rock") draw ;
-   if (choice === "paper" && cpuMove === "paper") draw;
-   if (choice === "scissors" && cpuMove === "scissors") draw; 
-   if (draw) console.log("draw");
+   if (choice === "rock" && cpuMove === "scissors")console.log("player win");
+   if (choice === "paper" && cpuMove === "rock")console.log("player win");
+   if (choice === "scissors" && cpuMove === "paper")console.log("player win");
+   
 
-   if (choice === "rock" && cpuMove === "scissors")  playerWin;
-   if (choice === "paper" && cpuMove === "rock")   playerWin;
-   if (choice === "scissors" && cpuMove === "paper")  playerWin;
-   if (playerWin) console.log("player win");
-
-   if (choice === "rock" && cpuMove === "paper")  cpuWin;
-   if (choice === "paper" && cpuMove === "scissors")  cpuWin;
-   if (choice === "scissors" && cpuMove === "rock")  cpuWin; 
-   if (cpuWin) console.log("cpu win");
-  
-  
-  
+   if (choice === "rock" && cpuMove === "paper")console.log("cpu win");
+   if (choice === "paper" && cpuMove === "scissors")console.log("cpu win");
+   if (choice === "scissors" && cpuMove === "rock")console.log("cpu win");
+      
 
 }
 
 
 
-
-computerPlay();
 playerSelection();
+computerPlay();
 playRound();
 
 
