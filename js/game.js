fa341cf8ac9cd16f721ea47bb4ac5a31b4954b90
James <jamesncoupland@gmail.com>
2021-09-28 15:56:29 +0100

    fixed outcome, single round works

--- js/game.js
+++ js/game.js
@@ -3,93 +3,71 @@ let playerScore = 0;
 let cpuScore = 0;
 
 
-function computerNumber() {
+function computerPlay() {
 
-    const computerRandom = Math.floor(Math.random()*3);
-
-    return computerRandom;
-
+    computerSelection =  Math.floor(Math.random()*3);
+   
+         
+    if (computerSelection === 0) { 
+        return "rock";
+    }   
+    else if (computerSelection === 1) {
+        return "paper";
+    }   
+    else { 
+        return "scissors"; 
+   }
+   
 }
 
-
-
-function computerPlay()
-{
-
-      const computerplayerSelection = computerNumber();
-     
-        if (computerplayerSelection === 0) { 
-    return "rock";
-    }   else if (computerplayerSelection === 1) {
-    return "paper";
-    }   else { 
-    return "scissors";
-}
-  
-}
-
-
-    
-
-//playerSelection ask for input rps into console then accept input of rps
-
-
-function playerSelection() {
-    const playerMove = prompt("rock paper scissors").toLowerCase();
-
-        if (playerMove === "rock") {
-    return "rock";
-    }   else if (playerMove === "paper") {
-    return "paper";
-    }   else if (playerMove === "scissors") {
-    return "scissors"
-    } 
-        
-  }
    
  
 
-function playRound(playerSelection, computerPlay) {
-           
-      if (playerSelection === "rock" && computerPlay === "scissors") {
-    return "player win"
-    }   else if (playerSelection === "paper" && computerPlay === "rock") {
-    return "player win"
-   }    else if (playerSelection === "scissors" && computerPlay === "paper") {
-       return "player win"
-   }    else if (playerSelection === "rock" && computerPlay === "paper") {
-   return "cpu win"
-   }    else if (playerSelection === "paper" && computerPlay === "scissors") {
-   return "cpu win"
-   }    else if (playerSelection === "scissors" && computerPlay === "rock") {
-   return "cpu win"
-   }   
+ function playRound(computerMove, playerMove) {   
+
+    computerMove = computerPlay();
+    
+    playerMove = prompt("rock paper scissors").toLowerCase();
+    console.log("player move: "+playerMove);
+    console.log("cpu move: "+computerMove)
+    
+
+    if (playerMove === computerMove) {
+        return "draw";
+    }
+    else if (playerMove === "rock" && computerMove === "scissors") {
+        return "player win";
+    }
+    else if (playerMove === "paper" && computerMove === "rock") {
+        return "player win";
+    }   
+    else if (playerMove === "scissors" && computerMove === "paper") { 
+        return "player win";
+    }
+    else if (playerMove === "rock" && computerMove === "paper") {
+        return "cpu win";
+    }    
+    else if (playerMove === "paper" && computerMove === "scissors") {
+        return "cpu win";
+    }  
+    else if (playerMove === "scissors" && computerMove === "rock") {
+        return "cpu win";
+    }   
+    else { alert ("OH NO!")
+}
    
-   
 }
 
 
 
-function score(playRound) {
-
-    if (playRound === "player win") { 
-        playerScore =+1
-}   else if  (playRound === "cpu win") {
-        cpuScore +=1
-}
 
 
-}
 
 
-playRound;
 
-console.log("cpu move: "+computerPlay())
-console.log("player move: "+playerSelection())
 console.log("round outcome: "+playRound())
 
 
-console.log(playerScore,cpuScore)
 
 
 
